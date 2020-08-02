08fe4f309c986bf9a8082b8b5c793ea40a919b43
HikawaRin <releaseavalon@gmail.com>
2020-08-02 17:40:04 +0800

    feat: megengine commit style

---
+++ README.md
@@ -0,0 +1,3 @@
+# commitlint-config-MegEngine  
+
+A shareable [conventional-changelog-lint](https://github.com/conventional-changelog/commitlint) configuration for [MegEngine](https://github.com/MegEngine/MegEngine) commit convention.  
---
+++ index.js
@@ -0,0 +1,44 @@
+const types = [
+	'feat',
+	'fix',
+	'perf',
+	'docs',
+	'style',
+	'refactor',
+	'test',
+	'build',
+	'ci',
+];
+
+const scopes = [
+    'mgb',
+    'mge',
+    'dnn',
+];
+
+module.exports = {
+	plugins: ['commitlint-plugin-regexscope'],
+    "rules":{
+        'body-leading-blank': [1, 'always'],
+		'footer-leading-blank': [1, 'always'],
+		'header-max-length': [2, 'always', 100],
+        'scope-case': [2, 'always', 'lower-case'],
+        'scope-empty': [2, 'never'],
+		'regexscope': [2, 'always', {
+            pattern: '^.*/?', 
+            clean: '^[^/]*', 
+            enum: scopes,
+            case: 'lower-case'
+        }],
+		'subject-case': [
+			2,
+			'never',
+			['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+		],
+		'subject-empty': [2, 'never'],
+		'subject-full-stop': [2, 'never', '.'],
+		'type-case': [2, 'always', 'lower-case'],
+		'type-empty': [2, 'never'],
+		'type-enum': [2, 'always', types],
+    },
+};
